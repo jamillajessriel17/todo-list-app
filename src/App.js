ceb9f0c80561e87a8b1a30478918a5c60d435114
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-30 11:19:03 +0800

    feat: add navigation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import TodoList from "./components/TodoList";
 function App() {
   return (
     <div className="App">
+      <div className="nav-bar">
+        <nav>
+          <ul>
+            <li>
+              <a href={"/"}>HOME</a>
+            </li>
+            <li>
+              <a href={"/done"}>Done List</a>
+            </li>
+            <li>
+              <a href={"/help"}>Help</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <TodoList />
     </div>
   );
