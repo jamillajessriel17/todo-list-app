025545e0dbd6965ade3d01387e0b3dff6c692592
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-31 00:38:39 +0800

    feat: add ant d style in todoItem details

--- src/components/TodoItemDetail.js
+++ src/components/TodoItemDetail.js
@@ -1,5 +1,7 @@
+import { Card } from "antd";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import "../css/todoDetail.css";
 
 const TodoItemDetail = () => {
   const { id } = useParams();
@@ -7,11 +9,21 @@ const TodoItemDetail = () => {
     state.todo.todos.find((task) => task.id === id)
   );
   return (
-    <div className="todo-detail">
-      <h2>TODO ITEM DETAILS</h2>
-      <div>Id: {todoTask?.id}</div>
-      <div>Name: {todoTask?.text}</div>
-    </div>
+    // <div className="todo-detail">
+    //   <h2>TODO ITEM DETAILS</h2>
+    //   <div>Id: {todoTask?.id}</div>
+    //   <div>Name: {todoTask?.text}</div>
+    // </div>
+    <>
+      <h2>TODO DETAILS</h2>
+      <Card
+        title={`ID: ${todoTask?.id}`}
+        bordered={false}
+        className="todoDetail"
+      >
+        NAME: {todoTask?.text}
+      </Card>
+    </>
   );
 };
 export default TodoItemDetail;
